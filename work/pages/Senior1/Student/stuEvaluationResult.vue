9c1098b26ff7b29bd37bcb36708918799af0576a
nutfyb <56430739+nutfyb@users.noreply.github.com>
2021-11-06 21:45:52 +0700

    create component of evaluation result page

---
+++ work/pages/Senior1/Student/stuEvaluationResult.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Appbar />
+    <Sidebar />
+ 
+    <EvaluationResultForm />
+    </div>
+</template>
+
+<script>
+import Appbar from "@/components/Global/mainAppbar";
+import Sidebar from "@/components/Student/stuSidebar";
+import EvaluationResultForm from "@/components/Student/stuEvaluationResultTable";
+export default {
+  components: {
+    Appbar,
+    Sidebar,
+    EvaluationResultForm
+  },
+};
+</script>
