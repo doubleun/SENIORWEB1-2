62f1ccb0ef4c4803c910cff3c7f3d26105f0a9f1
mnyspx <57520286+mnyspx@users.noreply.github.com>
2021-11-06 14:59:17 +0700

    Change routes student sidebar

--- work/components/Student/stuSidebar.vue
+++ work/components/Student/stuSidebar.vue
@@ -37,38 +37,6 @@
           </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
-      <!-- Sub group of List Final Documentation -->
-      <v-list-group no-action prepend-icon="mdi-numeric-8-box" color="white">
-        <template v-slot:activator>
-          <v-list-item-title>Final Documentation</v-list-item-title>
-        </template>
-        <v-list-item
-          v-for="(item, i) in documents"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-title v-text="item.title"></v-list-item-title>
-        </v-list-item>
-      </v-list-group>
-      <!-- List Re-evaluation -->
-      <v-list-item-group color="white">
-        <v-list-item
-          v-for="(item, i) in reevaluations"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -80,65 +48,64 @@ export default {
         {
           icon: "mdi-home",
           title: "Home",
-          to: "/"
+          to: "/",
         },
         {
           icon: "mdi-numeric-1-box",
           title: "Group",
-          to: "/Student/stuCreateGroup"
+          to: "/Senior1/Student/stuCreateGroup",
         },
         {
           icon: "mdi-numeric-2-box",
           title: "Topic",
-          to: "/Student/stuTopicEvaluationForm"
+          to: "/Senior1/Student/stuTopicEvaluationForm",
         },
         {
           icon: "mdi-numeric-3-box",
           title: "Proposal",
-          to: "/Student/stuProposalEvaluationForm"
+          to: "/Senior1/Student/stuProposalEvaluationForm",
         },
         {
           icon: "mdi-numeric-4-box",
           title: "Progress 1",
-          to: "/Student/stuProgress1EvaluationForm"
+          to: "/Senior1/Student/stuProgress1EvaluationForm",
         },
         {
           icon: "mdi-numeric-5-box",
           title: "Progress 2",
-          to: "/Student/stuProgress2EvaluationForm"
+          to: "/Senior1/Student/stuProgress2EvaluationForm",
         },
         {
           icon: "mdi-numeric-6-box",
           title: "Progress 3",
-          to: "/Student/stuProgress3EvaluationForm"
+          to: "/Senior1/Student/stuProgress3EvaluationForm",
         },
         {
           icon: "mdi-numeric-7-box",
+          title: "Progress 4",
+          to: "/Senior1/Student/stuProgress3EvaluationForm",
+        },
+        {
+          icon: "mdi-numeric-8-box",
           title: "Final Presentation",
-          to: "/Student/stuFinalPresentationEvaluationForm"
-        }
-      ],
-      documents: [
-        {
-          title: "Abstract",
-          to: "/Student/stuAbstractForm"
+          to: "/Senior1/Student/stuFinalPresentationEvaluationForm",
         },
         {
-          title: "Full Document",
-          to: "/Student/stuFullDocumentForm"
+          icon: "mdi-numeric-9-box",
+          title: "Final Documentation",
+          to: "/Senior1/Student/stuFinalDocumentationEvaluationForm",
         },
         {
-          title: "Other Resource",
-          to: "/Student/stuOtherResourceForm"
-        }
-      ],
-      reevaluations: [
+          icon: "mdi-file",
+          title: "Evaluation Result",
+          to: "/Senior1/Student/stuEvaluationResult",
+        },
         {
           icon: "mdi-file-refresh",
           title: "Re - evaluation",
-          to: "/Student/stuReevaluationForm"
-        }
-      ]
+          to: "/Senior1/Student/stuReEvaluation",
+        },
+      ],
     };
   },
   computed: {
@@ -148,8 +115,8 @@ export default {
       },
       set(newVal) {
         this.$store.commit("set_drawer", newVal);
-      }
-    }
-  }
+      },
+    },
+  },
 };
 </script>
