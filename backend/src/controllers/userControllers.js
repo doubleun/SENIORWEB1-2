0fbaf86a7972c1942e336e1116d95d7c0f0335bd
mukuro557 <6131302005@lamduan.mfu.ac.th>
2021-11-19 06:01:12 +0700

    เพิ่มชื่อที่ไม่ซ้ำ

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -1,7 +1,8 @@
 const con = require("../config/db");
 var xlsx = require('node-xlsx');
 const fs = require('fs');
-const readXlsxFile = require('read-excel-file/node')
+const readXlsxFile = require('read-excel-file/node');
+const { timeStamp } = require("console");
 
 
 testuser = async (req, res) => {
@@ -42,12 +43,11 @@ uploadfile = async (req, res) => {
             //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
             let avatar = req.files[''];
 
-
             //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            avatar.mv('uploads/' + avatar.name);
+            avatar.mv('uploads/' +Date.now()+ "_" + avatar.name);
             var data = []
             let count = 0;
-            var sql = "INSERT INTO `users` (`User_ID`, `User_Name`, `User_Email`, `User_Phone`, `User_Role`, `Major_ID`) VALUES (?)";
+            var sql = "INSERT INTO `users` (`User_ID`, `User_Identity_ID`, `User_Name`, `User_Email`, `User_Phone`, `User_Role`, `Major_ID`) Select ?,?,?,?,?,?,? Where not exists(select `User_ID`, `User_Identity_ID`, `User_Name`, `User_Email`, `User_Phone`, `User_Role`, `Major_ID` from users where User_ID= ?);";
             var obj = readXlsxFile("uploads/" + avatar.name).then((rows) => {
                 for (let i = 1; i < rows.length; i++) {
                     if (rows[i][4] == 'teacher') {
@@ -64,24 +64,25 @@ uploadfile = async (req, res) => {
                     } else if (rows[i][5] == 'ICE') {
                         rows[i][5] = 1
                     }
-                    // data.push(rows[i])
-                    con.query(sql, [rows[i]], (err, result, fields) => {
+                    // data.push(rows[i])]
+                    // console.log(rows[i][0])
+                    con.query(sql, [rows[i][0],rows[i][0],rows[i][1],rows[i][2],rows[i][3],rows[i][4],rows[i][5], rows[i][0]], (err, result, fields) => {
                         if (err) {
                             console.log(err.code)
-                            if(err.code == "ER_DUP_ENTRY"){
+                            if (err.code == "ER_DUP_ENTRY") {
                                 res.status(500).send("Duplicate data");
-                            }else{
+                            } else {
                                 res.status(500).send("Internal Server Error");
                             }
                         } else {
                             count++
-                            if (count == rows.length-1) {
-                    res.status(200).send("success")
-                }
+                            if (count == rows.length - 1) {
+                                res.status(200).send("success")
+                            }
                         }
                     });
                 }
-                
+
 
             })
 
