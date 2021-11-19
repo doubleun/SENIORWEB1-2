7c40a95173212d0fab6ab2fe399e318b86725f16
mukuro557 <6131302005@lamduan.mfu.ac.th>
2021-11-20 06:41:17 +0700

    add insert delete user

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -2,7 +2,7 @@ const con = require("../config/db");
 var xlsx = require('node-xlsx');
 const fs = require('fs');
 const readXlsxFile = require('read-excel-file/node');
-const { timeStamp } = require("console");
+const { result } = require("lodash");
 
 
 testuser = async (req, res) => {
@@ -22,14 +22,70 @@ getAllUser = async (req, res) => {
         }
     });
 
-    // console.log(data.result)
+}
 
-    // console.log(err.sqlMessage)
-    // res.status(500).send("Internal Server Error");
+addoneUser = async (req, res) => {
+    const User_Identity_ID = req.body.User_Identity_ID
+    const User_Name = req.body.User_Name
+    const User_Email = req.body.User_Email
+    const User_Phone = req.body.User_Phone
+    const User_Role = req.body.User_Role
+    const Major_ID = req.body.Major_ID
+    var sql = "INSERT INTO `users` ( `User_Identity_ID`, `User_Name`, `User_Email`, `User_Phone`, `User_Role`, `Major_ID`) Select ?,?,?,?,?,?,? Where not exists(select `User_ID`, `User_Identity_ID`, `User_Name`, `User_Email`, `User_Phone`, `User_Role`, `Major_ID` from users where User_Email= ?);";
 
-    // con.query(sql,(err,result,fields)=>{
-    //     if()
-    // })
+    await con.query(sql, [ User_Identity_ID, User_Name, User_Email, User_Phone, User_Role, Major_ID, User_Email], (err, result, fields) => {
+        if (err) {
+            console.log(err.code)
+            if (err.code == "ER_DUP_ENTRY") {
+                res.status(500).send("Duplicate data");
+            } else {
+                res.status(500).send("Internal Server Error");
+            }
+        } else {
+
+            res.status(200).send("success")
+
+        }
+    });
+
+}
+
+editoneUser = async (req, res) => {
+    const User_Identity_ID = req.body.User_Identity_ID
+    const User_Name = req.body.User_Name
+    const User_Email = req.body.User_Email
+    const User_Phone = req.body.User_Phone
+    const User_Role = req.body.User_Role
+    const Major_ID = req.body.Major_ID
+    var sql = "UPDATE `users` SET `User_Identity_ID`=?,`User_Name`=?,`User_Email`=?,`User_Phone`=?,`User_Role`= ?,`Major_ID`=? WHERE `User_Email`=?";
+
+    await con.query(sql, [ User_Identity_ID, User_Name, User_Email, User_Phone, User_Role, Major_ID, User_Email], (err, result, fields) => {
+        if (err) {
+            console.log(err.code)
+            if (err.code == "ER_DUP_ENTRY") {
+                res.status(500).send("Duplicate data");
+            } else {
+                res.status(500).send("Internal Server Error");
+            }
+        } else {
+
+            res.status(200).send("success")
+
+        }
+    });
+
+}
+
+deleteuser = async(req,res) =>{
+    const User_Email = req.body.User_Email
+    const sql = "DELETE FROM `users` WHERE `users`.`User_Email` = ?"
+    await con.query(sql,[User_Email],(err,result,fields) =>{
+        if (err) {
+            console.log(err.code)
+        } else {
+            res.status(200).send("success")
+        }
+    })
 }
 
 uploadfile = async (req, res) => {
@@ -44,10 +100,10 @@ uploadfile = async (req, res) => {
             let avatar = req.files[''];
 
             //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            avatar.mv('uploads/' +Date.now()+ "_" + avatar.name);
+            avatar.mv('uploads/' + Date.now() + "_" + avatar.name);
             var data = []
             let count = 0;
-            var sql = "INSERT INTO `users` (`User_ID`, `User_Identity_ID`, `User_Name`, `User_Email`, `User_Phone`, `User_Role`, `Major_ID`) Select ?,?,?,?,?,?,? Where not exists(select `User_ID`, `User_Identity_ID`, `User_Name`, `User_Email`, `User_Phone`, `User_Role`, `Major_ID` from users where User_ID= ?);";
+            var sql = "INSERT INTO `users` (`User_Identity_ID`, `User_Name`, `User_Email`, `User_Phone`, `User_Role`, `Major_ID`) Select ?,?,?,?,?,?,? Where not exists(select `User_ID`, `User_Identity_ID`, `User_Name`, `User_Email`, `User_Phone`, `User_Role`, `Major_ID` from users where User_Email= ?);";
             var obj = readXlsxFile("uploads/" + avatar.name).then((rows) => {
                 for (let i = 1; i < rows.length; i++) {
                     if (rows[i][4] == 'teacher') {
@@ -66,7 +122,7 @@ uploadfile = async (req, res) => {
                     }
                     // data.push(rows[i])]
                     // console.log(rows[i][0])
-                    con.query(sql, [rows[i][0],rows[i][0],rows[i][1],rows[i][2],rows[i][3],rows[i][4],rows[i][5], rows[i][0]], (err, result, fields) => {
+                    con.query(sql, [ rows[i][0], rows[i][1], rows[i][2], rows[i][3], rows[i][4], rows[i][5], rows[i][2]], (err, result, fields) => {
                         if (err) {
                             console.log(err.code)
                             if (err.code == "ER_DUP_ENTRY") {
@@ -97,6 +153,8 @@ uploadfile = async (req, res) => {
 
 module.exports = {
     getAllUser,
-    testuser,
+    addoneUser,
     uploadfile,
+    deleteuser,
+    editoneUser,
 };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const UserControllers = require("../controllers/userControllers")
-const path = require('path');
 
 var userRouter = require('express').Router();
 
-userRouter.get("/", UserControllers.testuser);
+// userRouter.get("/", UserControllers.addoneUser);
 userRouter.get("/getalluser", UserControllers.getAllUser);
 userRouter.post("/importuser", UserControllers.uploadfile);
-userRouter.post('/upload-avatar', UserControllers.uploadfile);
+userRouter.post("/adduser", UserControllers.addoneUser);
+userRouter.delete("/deleteuser", UserControllers.deleteuser)
+userRouter.put("/edituser", UserControllers.editoneUser)
 
 module.exports = userRouter;
