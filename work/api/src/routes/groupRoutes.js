3ec0e041733d48ed6e0f1431ac5ce915160c14e7
mukuro557 <6131302005@lamduan.mfu.ac.th>
2021-12-20 06:07:16 +0700

    creategroup and accept

--- work/api/src/routes/groupRoutes.js
+++ work/api/src/routes/groupRoutes.js
@@ -2,5 +2,8 @@ const groupRouter = require('express').Router()
 const groupController = require('../controllers/groupControllers')
 
 groupRouter.get('/all', groupController.getAll)
+groupRouter.post('/createGroup', groupController.createGroup)
+groupRouter.put('/statusgroup', groupController.statusgroup)
+
 
 module.exports = groupRouter
