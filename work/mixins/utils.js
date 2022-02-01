94c630b3f43b9b654b1b1e7e719d71edb829c8e3
Wachirachai Nitsomboon <nitsomboon77@gmail.com>
2022-02-01 14:55:52 +0700

    Working on the field validation function

--- work/mixins/utils.js
+++ work/mixins/utils.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 export default {
   methods: {
+    // * Data manipulations
     // Strucured clone
     handleClone(obj) {
       return _.clone(obj);
@@ -9,5 +10,35 @@ export default {
     handleCloneDeep(obj) {
       return _.cloneDeep(obj);
     },
-  },
+    // * Validations
+    handleValidateTextField(
+      string = "",
+      option = "onlyNormalCharEng",
+      required = true,
+      errorMsg = "Invalid value",
+      ...customValidations
+    ) {
+      // Destructure custom validations (allow up to 3 conditions)
+      const [cv1 = false, cv2 = false, cv3 = false] = customValidations;
+      // Evaluate custom validations
+      const cvAll = cv1 || cv2 || cv3;
+
+      switch (option) {
+        case "onlyNormalCharEng":
+          return (required && !string) || cvAll || !/^[A-Za-z]+$/.test(string)
+            ? errorMsg
+            : true;
+          break;
+        case "onlyNormalCharTh":
+          return (required && !string) || cvAll || !/[\p{Thai} ]+/.test(string)
+            ? errorMsg
+            : true;
+        case "onlyNumber":
+          return (required && !string) || cvAll || !/^[0-9]+$/.test(string)
+            ? errorMsg
+            : true;
+          break;
+      }
+    }
+  }
 };
