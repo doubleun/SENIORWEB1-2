39361ebf3cc0137d711043145f90972485d71e33
doubleun <nitsomboon77@gmail.com>
2022-03-10 17:58:49 +0700

    remove api middleware for now

--- work/senior/api/src/routes/assignmentRoutes.js
+++ work/senior/api/src/routes/assignmentRoutes.js
@@ -4,34 +4,37 @@ const multer = require("../controllers/multer");
 const middle = require("../middleware/middle");
 
 assignmentRouter.post(
-  "/uploadAssignments",middle.checkAuthenticated,
+  "/uploadAssignments",
   multer.upload.array("files", 10),
   assignmentController.uploadAssignments
 ); // student, teacher
 assignmentRouter.post(
-  "/getAssignmentFiles",middle.checkAuthenticated,
+  "/getAssignmentFiles",
   assignmentController.getAssignmentFiles
 ); // student, teacher
 assignmentRouter.post(
-  "/giveProgressScore",middle.checkAuthenticated,
+  "/giveProgressScore",
   multer.upload.single("file"),
   assignmentController.giveProgressScore
 ); // teacher
 assignmentRouter.post(
-  "/getTeacherProgressScore",middle.checkAuthenticated,
+  "/getTeacherProgressScore",
   assignmentController.getTeacherProgressScore
 ); // teacher
 assignmentRouter.post(
-  "/getEvaluationScores",middle.checkAuthenticated,
+  "/getEvaluationScores",
   assignmentController.getEvaluationScores
 ); // teacher
 // assignmentRouter.get("/major", criteriaController.getByMajor); // Add new score criterias
 // assignmentRouter.post("/edit", criteriaController.editCriteria); // admin
 assignmentRouter.post("/", assignmentController.getAssignment);
 
-assignmentRouter.post("/countFile",middle.checkAuthenticated, assignmentController.countFileByMajor);
+assignmentRouter.post("/countFile", assignmentController.countFileByMajor);
 assignmentRouter.get("/abstracts", assignmentController.getAbstracts);
-assignmentRouter.post("/groupAssignment",middle.checkAuthenticated, assignmentController.getGroupAssignment);
+assignmentRouter.post(
+  "/groupAssignment",
+  assignmentController.getGroupAssignment
+);
 
 // assignmentRouter.post("/getBymajor", assignmentController.getAssignmentFilesMajor);
 
--- work/senior/api/src/routes/userRoutes.js
+++ work/senior/api/src/routes/userRoutes.js
@@ -6,23 +6,23 @@ const middle = require("../middleware/middle");
 var userRouter = require("express").Router();
 
 userRouter.get("/", UserControllers.getUser);
-userRouter.post("/getAllUserWithMajor",middle.checkAuthenticated, UserControllers.getAllUserWithMajor);
+userRouter.post("/getAllUserWithMajor", UserControllers.getAllUserWithMajor);
 // Fluke made this
-userRouter.post("/getAllUsersInSchool",middle.checkAuthenticated, UserControllers.getAllUsersInSchool);
+userRouter.post("/getAllUsersInSchool", UserControllers.getAllUsersInSchool);
 // userRouter.post("/gettacherwithrole", UserControllers.getTachersWithRole);
-userRouter.post("/amount",middle.checkAuthenticated, UserControllers.countUser);
+userRouter.post("/amount", UserControllers.countUser);
 userRouter.post(
-  "/importstudent",middle.checkAuthenticatedteacher,
+  "/importstudent",
   multer.uploadUser.array("files", 10),
   UserControllers.uploadfile
 );
 userRouter.post(
-  "/importteacher",middle.checkAuthenticatedteacher,
+  "/importteacher",
   multer.uploadUser.array("files", 10),
   UserControllers.uploadfileteacher
 );
 userRouter.get("/getAllMajors", UserControllers.getAllMajors); // admin
-userRouter.get("/getTeacherRole",middle.checkAuthenticated, UserControllers.getTeacherRole); // admin
-userRouter.post("/updateUserRole",middle.checkAuthenticatedteacher, UserControllers.updateUserRole); // admin
+userRouter.get("/getTeacherRole", UserControllers.getTeacherRole); // admin
+userRouter.post("/updateUserRole", UserControllers.updateUserRole); // admin
 
 module.exports = userRouter;
