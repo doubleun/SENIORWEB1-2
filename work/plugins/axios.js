b1192791fa750704773d5836422225cf3117ed21
doubleun <nitsomboon77@gmail.com>
2022-04-09 15:33:21 +0700

    update axios onError

--- work/plugins/axios.js
+++ work/plugins/axios.js
@@ -1,14 +1,23 @@
-export default ({ app, $axios, redirect }) => {
+export default ({ app, $axios, redirect, error: nuxtError }) => {
   // TODO: Add redirect after error or something
   $axios.onError((error) => {
     console.log("axios error response", error.response);
     const status = error.response.status;
-    console.log("status", status);
+    // console.log("status", status);
     if (status !== 200) {
       let errMsg = error.message || "An error has occured";
       if (!!error.response.data && typeof error.response.data === "string")
         errMsg = error.response.data;
-      app.$swal.fire("Error: " + status, errMsg, "warning");
+
+      if (!!app.$swal) {
+        app.$swal.fire("Error: " + status, errMsg, "warning");
+      }
+
+      nuxtError({
+        statusCode: status,
+        message: errMsg,
+      });
+      return Promise.resolve(false);
     }
   });
 };
