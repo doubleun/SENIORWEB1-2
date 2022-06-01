0a2ed4694e0d131ff55fb1c56b6a9d1ee87e5f88
Tee <cickpoo0121@gmail.com>
2022-06-01 21:23:37 +0700

    give string as a score

--- senior/api/src/config/db.js
+++ senior/api/src/config/db.js
@@ -3,20 +3,20 @@ const mysql = require("mysql2");
 // const config = require("./src/config/db");
 
 //! OLD
-// let connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_DATABASE,
-//   port: process.env.DB_PORT || 3308,
-// });
-
-const pool = mysql.createPool({
+let connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT || 3308
+  port: process.env.DB_PORT || 3308,
 });
 
-module.exports = pool;
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_DATABASE,
+//   port: process.env.DB_PORT || 3308
+// });
+
+module.exports = connection;
